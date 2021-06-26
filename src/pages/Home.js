9d54b752e7841b24c2c5b4f3ee344ec9581d4b40
Sandyabhi <abhisandy47@gmail.com>
2021-06-26 16:32:26 +0530

    aa

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import axios from "axios"
+import "./Home.css"
 
 function Home() {
 
@@ -7,6 +9,20 @@ function Home() {
     const onChangePicture = e => {
         setPicture(URL.createObjectURL(e.target.files[0]));
     };
+
+    const submitData = () => {
+        const fd = new FormData();
+        fd.append('image', picture)
+        //url = backend or ML url (ex: localhst: 4000, https://ML.netlify.com)
+        axios.post("url", fd, {
+            onUploadProgress: progressEvent => {
+                console.log("upload progress" + (progressEvent.loaded / progressEvent.total) * 100 + "%")
+            }
+        })
+            .then(res => {
+                console.log(res);
+            })
+    }
 
 
     return (
